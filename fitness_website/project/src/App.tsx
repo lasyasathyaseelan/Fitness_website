9e4f9653fb764a5551156d27a3f0081b144a90ac
lasyasathyaseelan <lasyasathyaseelan2005@gmail.com>
2025-06-30 10:11:11 +0530

    Update App.tsx

--- fitness_website/project/src/App.tsx
+++ fitness_website/project/src/App.tsx
@@ -7,6 +7,7 @@ import Home from './pages/Home';
 import Shop from './pages/Shop';
 import ProductDetail from './pages/ProductDetail';
 import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
 import Quiz from './pages/Quiz';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -23,6 +24,7 @@ function App() {
               <Route path="/shop" element={<Shop />} />
               <Route path="/product/:id" element={<ProductDetail />} />
               <Route path="/cart" element={<Cart />} />
+              <Route path="/checkout" element={<Checkout />} />
               <Route path="/quiz" element={<Quiz />} />
               <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} />
